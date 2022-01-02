121dc132801b0c8a47f93c058cfbc493a4c4e3c6
Marcel Iskrač <40259973+iskraM@users.noreply.github.com>
2022-01-02 22:32:33 +0100

    Prepared D22

---
+++ algorithm/Drone.h
@@ -0,0 +1,63 @@
+#ifndef DRONE
+#define DRONE
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+#include "Point.h"
+
+using namespace std;
+
+class Drone {
+private:
+	Point startPosition;
+	Point endPosition;
+	Point currentPosition;
+	vector <Point> allCoordinates;
+	vector <Point> pathToEnd;
+	vector <vector<bool>> edges;
+
+	int index = -1;
+	bool end;
+public:
+	Drone();
+	Drone(Point startPos, Point endPos, std::vector<Point> allPos);
+	Drone(Point startPos, Point endPos);
+	//virtual ~Drone();
+
+	Point getCurrentPosition() const;
+
+	Point getStartPosition() const;
+	Point getEndPosition() const;
+
+	int getIndex() const;
+	bool isNext() const;
+	bool isEnd() const;
+	Point getNext();
+	Point getPrev();
+
+	void addCoordinate(Point coordinate);
+	void setCurrentPosition(Point currentPosition);
+	void setIndex(int index);
+	void setEnd(bool end);
+	void addAll(vector<Point> allPos);
+	void addToPathAtIndex(int i, Point p);
+	void backtrack(Point p);
+	void removeCoordinateAt(int i);
+
+	void addToPath(Point p);
+	int getPathSize();
+	Point getCoordinate(int i);
+
+	bool IsHovering();
+
+	void GenerateEdges();
+	bool ValidEdge(Point p);
+
+	bool IsAtEnd();
+
+	virtual std::string toString() const;
+};
+
+#endif
--- algorithm/main.cpp
+++ algorithm/main.cpp
@@ -1,32 +1,143 @@
-#include <iostream>
+﻿#include <iostream>
 #include <fstream>
+#include <vector>
+#include <climits>
+#include "Drone.h"
+#include "Point.h"
 
 using namespace std;
 
-int main(int argc, char *argv[])
+int GetValue(int start, int end) {
+	int value = start;
+	if (start < end) start = start + 1;
+	else start = start - 1;
+
+	// yield value
+	return value;
+}
+
+Point GetVertex(Point start, Point end) {
+	for (int x = 0; x < GetValue(start.getX(), end.getX()); x++) {
+		for (int y = 0; y < GetValue(start.getY(), end.getY()); y++) {
+			for (int z = 0; z < GetValue(start.getZ(), end.getZ()); z++) {
+				// yield CreateVertex();
+			}
+		}
+	}
+
+	return Point();
+}
+
+bool Feasible(Point a, Point b) {
+	// vsaj ena izmed koordinat mora biti različna
+	if (a.getX() != b.getX())
+		return true;
+	if (a.getY() != b.getY())
+		return true;
+	if (a.getZ() != b.getZ())
+		return true;
+	return false;
+}
+
+bool Valid(Point a, Point b) {
+	return sqrt(pow(b.getX() - a.getX(), 2) + pow(b.getY() - a.getY(), 2) + pow(b.getZ() - a.getZ(), 2)) == 1;
+}
+
+bool pointsOK(Point a, Point b, Point prevA, Point prevB) {
+	return (abs(prevA.getX() - a.getX()) <= 1 && abs(prevA.getY() - a.getY()) <= 1 && abs(prevA.getZ() - a.getZ()) <= 1 && abs(prevB.getX() - b.getX()) <= 1 && abs(prevB.getY() - b.getY()) <= 1 && abs(prevB.getZ() - b.getZ()) <= 1);
+}
+
+void D22(Drone& A, Drone& B) {
+	A.addToPath(GetVertex(A.getStartPosition(), A.getEndPosition()));
+	B.addToPath(GetVertex(B.getStartPosition(), B.getEndPosition()));
+
+	// accept starting points
+	A.addToPath(A.getStartPosition());
+	B.addToPath(B.getStartPosition());
+
+	Point a, b;
+	while (!A.IsAtEnd() && !B.IsAtEnd()) {
+		a = GetVertex(A.getStartPosition(), A.getEndPosition());
+		b = GetVertex(B.getStartPosition(), B.getEndPosition());
+
+		// TODO 
+	}
+
+
+	std::cout << endl << "Rezultat" << endl;
+	cout << "------------------------" << endl;
+	int ia = 0, ib = 0;
+	for (int i = 0; i < (int)min(A.getPathSize(), B.getPathSize()); i++) {
+		cout << A.getCoordinate(ia).toString() << " " << B.getCoordinate(ib).toString() << "\n";
+
+		if (ia + 1 < A.getPathSize()) ia++;
+		if (ib + 1 < B.getPathSize()) ib++;
+	}
+
+
+	// izpis za prikaz v Blenderju
+	/*cout << "A:" << endl;
+	for (size_t i = 0; i < (int)min(A.getPathSize(), B.getPathSize()); i++) {
+		cout << A.getCoordinate(i).toString() << ", ";
+	}
+
+	cout << "\nB:" << endl;
+	for (size_t i = 0; i < (int)min(A.getPathSize(), B.getPathSize()); i++) {
+		cout << B.getCoordinate(i).toString() << ", ";
+	}*/
+}
+
+
+void writeToFile(Drone A, Drone B) {
+	ofstream outFile("result.out");
+
+	int ia = 0, ib = 0;
+	for (int i = 0; i < (int)min(A.getPathSize(), B.getPathSize()); i++) {
+		outFile << A.getCoordinate(ia).toString() << " " << B.getCoordinate(ib).toString() << endl;
+
+		if (ia + 1 < A.getPathSize()) ia++;
+		if (ib + 1 < B.getPathSize()) ib++;
+	}
+
+	outFile.close();
+}
+
+int main(int argc, char* argv[])
 {
-    if (argc == 1)
-    {
-        cout << "Uporaba: ./main [datoteka]" << endl;
-        exit(1);
-    }
+	if (argc == 1) {
+		cout << "Uporaba: ./main [datoteka]" << endl;
+		exit(1);
+	}
 
-    ifstream file(argv[1], ios::in);
-    if (!file.is_open())
-    {
-        cerr << "Napaka: datoteke ni bilo mogoče odpreti." << endl;
-        exit(1);
-    }
+	ifstream file(argv[1], ios::in);
+	if (!file.is_open()) {
+		cerr << "Napaka: datoteke ni bilo mogoče odpreti." << endl;
+		exit(1);
+	}
 
-    int start_a[3], end_a[3];
-    int start_b[3], end_b[3];
+	int start_a[3], end_a[3];
+	int start_b[3], end_b[3];
 
-    file >> start_a[0] >> start_a[1] >> start_a[2];
-    file >> end_a[0] >> end_a[1] >> end_a[2];
+	// read input
+	file >> start_a[0] >> start_a[1] >> start_a[2];
+	file >> end_a[0] >> end_a[1] >> end_a[2];
 
-    file >> start_b[0] >> start_b[1] >> start_b[2];
-    file >> end_b[0] >> end_b[1] >> end_b[2];
+	file >> start_b[0] >> start_b[1] >> start_b[2];
+	file >> end_b[0] >> end_b[1] >> end_b[2];
 
-    file.close();
-    return 0;
+	file.close();
+
+	cout << "Working with: " << argv[1] << endl;
+
+	Drone droneA = Drone(Point(start_a), Point(end_a));
+	Drone droneB = Drone(Point(start_b), Point(end_b));
+
+	cout << droneA.toString() << endl;
+	cout << droneB.toString() << endl;
+
+	D22(droneA, droneB);
+
+	// writeToFile(droneA, droneB);
+
+	return 0;
 }
